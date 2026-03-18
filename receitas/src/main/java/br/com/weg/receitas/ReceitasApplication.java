34762363296b32b2a4c0608e8354391039fc8d10
ELIS JASPER <elis_jasper@estudante.sesisenai.org.br>
2026-03-18 15:13:07 -0300

    Feat: adiciona entidade Receita

--- receitas/src/main/java/br/com/weg/receitas/ReceitasApplication.java
+++ receitas/src/main/java/br/com/weg/receitas/ReceitasApplication.java
@@ -1,6 +1,5 @@
 package br.com.weg.receitas;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.run;
