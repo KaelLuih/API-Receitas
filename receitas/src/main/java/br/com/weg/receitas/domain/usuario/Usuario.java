a47fb38031886cee58d7b27975808a6819daff84
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-03-18 15:10:36 -0300

    MZ feat: implementa entidade Usuario

--- receitas/src/main/java/br/com/weg/receitas/domain/usuario/Usuario.java
+++ receitas/src/main/java/br/com/weg/receitas/domain/usuario/Usuario.java
@@ -1,4 +1,25 @@
 package br.com.weg.receitas.domain.usuario;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
 }
