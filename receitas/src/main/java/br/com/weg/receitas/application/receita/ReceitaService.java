3f5f74e2aafd218ffbda0b323c1b92bfaea455a1
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-03-19 21:13:51 -0300

    MZ feat: implementa filtros personalizados de busca para a entidade receita

--- receitas/src/main/java/br/com/weg/receitas/application/receita/ReceitaService.java
+++ receitas/src/main/java/br/com/weg/receitas/application/receita/ReceitaService.java
@@ -9,6 +9,9 @@ public interface ReceitaService {
 
     List<ReceitaRespostaDto> listarReceitas();
     ReceitaRespostaDto buscarPorId(Long id);
+    List<ReceitaRespostaDto> buscarPorNome(String nome);
+    List<ReceitaRespostaDto> buscarPorPorcao(double porcao);
+    List<ReceitaRespostaDto> buscarPorIngrediente(String ingrediente);
     ReceitaRespostaDto salvarReceita(ReceitaRequisicaoDto requisicaoDto);
     ReceitaRespostaDto atualizarReceita(Long id, ReceitaRequisicaoDto requisicaoDto);
     void deletarPorNome(String nome);
